c8a384a90668a19c144d71218a2352390bf00a7a
Matthew Courtney <matthewdc182@gmail.com>
2021-01-06 15:28:19 -0500

    comleted the daily javascript exersise very similar to yesterdays

--- index.js
+++ index.js
@@ -3,22 +3,40 @@
 // if arr1 min is greater than arr2 min and if arr1 max is less than arr2 max
 // Solve Time: 10min
 
-const canNest = function(arr1, arr2) {
-    // console.log(arr1)
-    // console.log(arr2)
+// const canNest = function(arr1, arr2) {
+//     // console.log(arr1)
+//     // console.log(arr2)
 
-    const arr1sorted = arr1.sort((a, b) => a - b);
-    const arr2sorted = arr2.sort((a, b) => a - b);
+//     const arr1sorted = arr1.sort((a, b) => a - b);
+//     const arr2sorted = arr2.sort((a, b) => a - b);
 
-    if(arr1sorted[0] < arr2sorted[0] && arr1sorted[arr1sorted.length -1] < arr2sorted[arr2sorted.length - 1]) {
-        console.log("false");
-        return false;
+//     if(arr1sorted[0] < arr2sorted[0] && arr1sorted[arr1sorted.length -1] < arr2sorted[arr2sorted.length - 1]) {
+//         console.log("false");
+//         return false;
         
-    }
+//     }
 
-    console.log("true")
-    return true
+//     console.log("true")
+//     return true
+// }
+
+// canNest([1,2], [3,4]);
+// canNest([10, 12], [15, 8]);
+
+
+// Find the smallest and biggest numbers
+// function takes in an array of numbers and returns borth the min and max numbers in that order
+// Solve Time: 10 min
+
+const minMax = (...nums) => {
+    console.log(nums);
+    const sorted = nums.sort((a,b) => a - b);
+    const resultArray = []
+    resultArray.push(sorted[0]);
+    resultArray.push(sorted[sorted.length - 1])
+    console.log(resultArray)
+
 }
 
-canNest([1,2], [3,4]);
-canNest([10, 12], [15, 8]);
+minMax(1,2,3,4,5)
+minMax(16, 8, 27, 45)
