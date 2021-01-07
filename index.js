2ec57f57a23b09bd140a634c59043dc33ac35cf8
Matthew Courtney <matthewdc182@gmail.com>
2021-01-07 18:31:44 -0500

    completed the daily js task sorts an array of drinks by price

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // function takes in two arrays as an argument 
 // if arr1 min is greater than arr2 min and if arr1 max is less than arr2 max
 // Solve Time: 10min
+// Date: 1/5/2021
 
 // const canNest = function(arr1, arr2) {
 //     // console.log(arr1)
@@ -27,16 +28,39 @@
 // Find the smallest and biggest numbers
 // function takes in an array of numbers and returns borth the min and max numbers in that order
 // Solve Time: 10 min
+// Date: 1/6/2021
 
-const minMax = (...nums) => {
-    console.log(nums);
-    const sorted = nums.sort((a,b) => a - b);
-    const resultArray = []
-    resultArray.push(sorted[0]);
-    resultArray.push(sorted[sorted.length - 1])
-    console.log(resultArray)
+// const minMax = (...nums) => {
+//     console.log(nums);
+//     const sorted = nums.sort((a,b) => a - b);
+//     const resultArray = []
+//     resultArray.push(sorted[0]);
+//     resultArray.push(sorted[sorted.length - 1])
+//     console.log(resultArray)
 
+// }
+
+// minMax(1,2,3,4,5)
+// minMax(16, 8, 27, 45)
+
+
+
+// Drink Sorting
+// Function takes in an array of objects. Each object has two properties name and cost. the function will take in the array and sort the ddrinks by price in ascending order
+// Solve Time: 5 min
+// Date: 1/7/2021
+
+const drinks = [
+    {name: "lemonade", price: 100},
+    {name: "milk", price: 70},
+    {name: "soda", price: 125}
+]
+
+function sortByPrice(drinks) {
+    drinks.sort((drink1,drink2) => drink1.price - drink2.price);
+    drinks.forEach((drink) => {
+        console.log(drink.name, drink.price)
+    })
 }
 
-minMax(1,2,3,4,5)
-minMax(16, 8, 27, 45)
+sortByPrice(drinks)
